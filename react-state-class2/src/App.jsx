04c86db57a06d3ff7af4d80e1f60486c7b74fa26
= <=>
2026-06-28 22:00:42 +0530

    Basic Lottery-Game added

--- react-state-class2/src/App.jsx
+++ react-state-class2/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import LudoBoard from "./LudoBoard.jsx"
 import TodoList from "./TodoList.jsx"
+import Lottery from "./Lottery.jsx"
 
 function App() {
 
   return(
     <>
-    <TodoList />
+    <Lottery />
     </>
   ) ;
   
